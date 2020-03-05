b9ead434ff121560c0dc85000ef2f4540a909e34
asntech <azez.khan@gmail.com>
2020-03-04 20:31:24 -0800

    updated doc version

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ copyright = u'2019, Aziz Khan'
 author = u'Aziz Khan'
 
 #from biasaway import __version__ as VERSION
-VERSION = '1.0.4'
+VERSION = '2.0.0'
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
